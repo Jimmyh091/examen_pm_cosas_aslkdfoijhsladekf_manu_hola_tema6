1501083c1c3c91e1bad4cf378aa318d276313d0d
Jimmyh091 <jaimecaballero164@gmail.com>
2024-12-05 18:53:15 +0100

    initial kufkuyfkygyghkuh

---
+++ settings.gradle.kts
@@ -0,0 +1,24 @@
+pluginManagement {
+    repositories {
+        google {
+            content {
+                includeGroupByRegex("com\\.android.*")
+                includeGroupByRegex("com\\.google.*")
+                includeGroupByRegex("androidx.*")
+            }
+        }
+        mavenCentral()
+        gradlePluginPortal()
+    }
+}
+dependencyResolutionManagement {
+    repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
+    repositories {
+        google()
+        mavenCentral()
+    }
+}
+
+rootProject.name = "examen_pm_cosas_aslkdfoijhsladekf_manu_hola_tema6"
+include(":app")
+ 
